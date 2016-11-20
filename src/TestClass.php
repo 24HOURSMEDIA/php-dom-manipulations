ef7134e0b95da2b87a8997b735575d52e7ae13f3
24hoursmedia <info@24hoursmedia.com>
2016-11-20 14:00:21 +0100

    test classes (temporary)

---
+++ src/TestClass.php
@@ -0,0 +1,10 @@
+<?php
+namespace T4\PHPDomManipulations;
+
+/**
+ * Date: 20/11/2016
+ */
+class TestClass
+{
+
+}
---
+++ src/V/V2.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Date: 20/11/2016
+ */
+
+namespace T4\PHPDomManipulations\V;
+
+
+class V2
+{
+
+}
